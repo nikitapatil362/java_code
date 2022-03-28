527f185b2a496237eac428f5424d66f001bd6cf8
nikitapatil362 <91883115+nikitapatil362@users.noreply.github.com>
2022-03-28 18:08:32 +0530

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,14 @@
+
+public class dataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+float A =1;
+float B =5;
+float C =A+B;
+float D =A-B;
+System.out.println("The addition of numbers is:"+C);
+System.out.println("The subtraction of numbers is:"+D);
+	}
+
+}
