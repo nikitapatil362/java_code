9f37c706e66968ffb9a0946f0b51d7013d5ea6d9
nikitapatil362 <91883115+nikitapatil362@users.noreply.github.com>
2022-03-28 22:20:16 +0530

    Add files via upload

---
+++ InchToMeters.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InchToMeters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Give an input value in inch ");
+Scanner sc = new Scanner(System.in);
+float A =sc.nextFloat();
+float meter =(float) (A*0.0254);
+System.out.println(A +" inch is " +meter+ " meters");
+	}
+
+}
+
