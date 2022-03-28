eebb0e093c63df282aaaa44608a6c934e9eb1c15
nikitapatil362 <91883115+nikitapatil362@users.noreply.github.com>
2022-03-28 18:07:50 +0530

    Add files via upload

---
+++ forLoopDemo.java
@@ -0,0 +1,17 @@
+
+public class forLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum =0;
+		for(int i =1; i<=10; i++)
+		{
+			sum =sum+i;
+			
+		}
+		System.out.println("The sum is "+sum);
+
+	}
+
+}
---
+++ whileLoopDemo.java
@@ -0,0 +1,17 @@
+
+public class whileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum =0;
+		int i =11;
+		while(int i<=10)
+				{
+			sum =sum+i;
+			i++;
+				}
+System.out.println("The sum is "+sum;)
+	}
+
+}
