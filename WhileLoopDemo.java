4b3f65b119d48af74bfc015c3e03bc14160c0b82
nikitapatil362 <91883115+nikitapatil362@users.noreply.github.com>
2022-03-28 23:33:04 +0530

    Add files via upload

---
+++ WhileLoopDemo.java
@@ -0,0 +1,18 @@
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum =0;
+		int i =1;
+		while( i<=10)
+				{
+			sum =sum+i;
+			i++;
+				}
+System.out.println("The sum is "+sum);
+	}
+
+}
+
