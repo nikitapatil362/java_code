652f7f8365c58345e769f60605f4282bfed9b5f9
nikitapatil362 <91883115+nikitapatil362@users.noreply.github.com>
2022-03-28 23:36:55 +0530

    Add files via upload

---
+++ GettingTwoNo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GettingTwoNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter first no.");
+Scanner sc = new Scanner(System.in);
+int A = sc.nextInt();
+System.out.println("Enter second no.");
+int B = sc.nextInt();
+int C =A+B;
+System.out.println("The sum of two no is :"+C);
+
+	}
+
+}
---
+++ StringHandeling.java
@@ -0,0 +1,11 @@
+
+public class StringHandeling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String A ="Hi everyone!";
+String B="My name is Nikita";
+System.out.println(A+B);
+	}
+
+}
